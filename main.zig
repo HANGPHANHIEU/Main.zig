d4403b1add58727ae63558f529b5cb1e0ce63d24
HangPhan.HuuCo <130420453+HANGPHANHIEU@users.noreply.github.com>
2026-02-19 15:47:56 +0700

    Update main.zig

--- main.zig
+++ main.zig
@@ -1,30 +1,8 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-
-    var server = std.http.Server.init(allocator, .{
-        .reuse_address = true,
-    });
-
-    const port = std.os.getenv("PORT") orelse "8080";
-    const parsed_port = try std.fmt.parseInt(u16, port, 10);
-
-
-    try server.listen(.{
-        .address = try std.net.Address.parseIp("0.0.0.0", port),
-    });
-
-    std.debug.print("Server running on port {d}\n", .{port});
-
-    while (true) {
-        var res = try server.accept(.{});
-        defer res.deinit();
-
-        try res.respond(
-            "Hello from Zig Online 🚀",
-            .{ .status = .ok },
-        );
-    }
+    try std.fs.File.stdout().writeAll("Hello, World!\n");
 }
+
+const port = std.os.getenv("PORT") orelse "8080";
+const parsed_port = try std.fmt.parseInt(u16, port, 10);
