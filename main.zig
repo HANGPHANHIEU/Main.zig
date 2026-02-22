68a17e48883e29bfaadcf0d48b837ec01917e85d
HangPhan.HuuCo <130420453+HANGPHANHIEU@users.noreply.github.com>
2026-02-22 19:39:37 +0700

    Update main.zig

--- main.zig
+++ main.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
 
 pub fn main() !void {
-    try std.fs.File.stdout().writeAll("Hello, World!\n");
+    std.debug.print("Hello, TikTok!\n", .{});
 }
-
-const port = std.os.getenv("PORT") orelse "8080";
-const parsed_port = try std.fmt.parseInt(u16, port, 10);
